54db111aa89875431099a5a1fdae693c06f93610
Dhanayush7 <99guptashubh@mail.com>
2026-08-08 22:10:47 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,17 +1,18 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-// Attach token from localStorage to every request if available
 API.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-        config.headers = config.headers || {};
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers = config.headers || {};
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
 
 export default API;
