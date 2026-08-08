8e9c134e9a2a74ceaf41af95e3bce2da697c4947
Dhanayush7 <99guptashubh@mail.com>
2026-08-08 21:05:07 +0530

    Update NotFound.jsx

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -4,9 +4,7 @@ import "../css/NotFound.css";
 function NotFound() {
   return (
     <div className="notfound">
-
       <div className="notfound-content">
-
         <h1>404</h1>
 
         <h2>Oops! Page Not Found</h2>
@@ -18,18 +16,14 @@ function NotFound() {
         <Link to="/">
           <button>🏠 Back to Home</button>
         </Link>
-
       </div>
 
       <div className="notfound-image">
-
         <img
           src="https://illustrations.popsy.co/gray/web-error.svg"
           alt="404"
         />
-
       </div>
-
     </div>
   );
 }
